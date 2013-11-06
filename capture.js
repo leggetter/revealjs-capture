acadc05d49d6c4dc226e1f5e0474b238763c2c8a
Phil Leggetter <phil@leggetter.co.uk>
2013-11-06 14:38:38 +0000

    Trying to make things a little more usable

--- capture.js
+++ capture.js
@@ -1,39 +1,54 @@
-console.log( 'in capture.js' );
+log( 'Capturing...' );
+
+var system = require( 'system' );
+
+var args = system.args;
+if( !args[ 1 ] ) {
+  log( 'Missing required capture arguments' );
+  phantom.exit();
+}
+
+var captureArgsJson = args[ 1 ];
+log( 'captureArgsJson', captureArgsJson );
+
+var captureArgs = JSON.parse( captureArgsJson );
+
+var slidesUrl = captureArgs.slides;
 
 var page = require('webpage').create();
 
 page.viewportSize = { width: 1600, height: 1200 };
 
-var pageToLoad = 'http://leggetter.github.io/talks/realtime-tech-stack/';
-
-console.log( 'opening page: ', pageToLoad );
+log( 'opening page: ', slidesUrl );
 
 var captureCount = 0;
+var pageResourceCount = 0;
 page.onLoadFinished = function() {
 
-  console.log( 'loadFinished' );
+  log( 'loadFinished' );
 
-  console.log( 'waiting 10 seconds after intial load' );
+  log( 'waiting 10 seconds after initial load' );
   setTimeout( capturePage, 10000 );
 };
 
 page.onResourceRequested = function() {
-  console.log( 'resourceRequested' );
-  printArgs.apply( this, arguments );
+  ++pageResourceCount;
+  // log( 'loaded', pageResourceCount, 'resources' );
 }
 
 page.onUrlChanged = function() {
+  pageResourceCount = 0;
   if( captureCount > 0 ) {
     setTimeout( capturePage, 5000 );
   }
 };
 
-page.open( pageToLoad, function () {
-  console.log( 'loaded: ', pageToLoad );
+page.open( slidesUrl, function () {
+  log( 'loaded: ', slidesUrl );
 } );
 
 function capturePage() {
-  console.log( 'capturing page' );
+  log( 'capturing page' );
   var slideIndices = page.evaluate( function() {
     return Reveal.getIndices();
   } );
@@ -41,13 +56,11 @@ function capturePage() {
 
   ++captureCount;
 
-  console.log( 'captured ', captureCount, ' pages' );
+  log( 'captured ', captureCount, ' pages' );
 
   var isLastSlide = page.evaluate( function() {
     return Reveal.isLastSlide();
   } );
-
-  console.log( 'last slide? ', isLastSlide );
 
   if( !isLastSlide ) {
     page.evaluate( function() {
@@ -59,10 +72,6 @@ function capturePage() {
   }
 }
 
-function printArgs() {
-    var i, ilen;
-    for (i = 0, ilen = arguments.length; i < ilen; ++i) {
-        console.log("    arguments[" + i + "] = " + JSON.stringify(arguments[i]));
-    }
-    console.log("");
+function log() {
+  console.log.apply( console, arguments );
 }
--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-// if (system.args.length === 1) {
-//     console.log('Usage: loadspeed.js <some URL>');
-//     phantom.exit();
-// }
 var path = require( 'path' );
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
 
+var program = require( 'commander' );
+
+program
+  .version('0.0.1')
+  .option('-s, --slides <url>', 'The url to the RevealJS slides')
+  .parse( process.argv );
+
+var captureArgs = {
+                    slides: program.slides
+                  };
+var captureArgsJson = JSON.stringify( captureArgs );
 var childArgs = [
-  path.join(__dirname, 'capture.js'),
-  'some other argument (passed to phantomjs script)'
+  path.join( __dirname, 'capture.js' ),
+  captureArgsJson
 ];
 
-console.log( 'kicking off child process' );
+console.log( 'Starting capture child process' );
 
 var child = childProcess.execFile( binPath, childArgs, function( err, stdout, stderr ) {
-  // handle results
-  console.log( arguments );
+  if( err ) {
+    console.error( err );
+  }
+  else {
+    console.log( 'Capture process complete' );
+  }
 } );
 
-console.log( child );
+child.stdout.on('data', function() {
+  console.log.apply( console, arguments );
+} );
